3c7f984211c3d506651fd45bded4b4c524e6c805
mariagrs <maria.graus1998@gmail.com>
2021-05-15 17:52:32 +0200

    Added Bootstrap links

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,25 +1,32 @@
 import React, {useState} from 'react'
 import logo from '../assets/logo.png'
 import '../css/styles.css'
-import {Navbar} from 'react-bootstrap'
+import {Navbar, Form, Button, FormControl} from 'react-bootstrap'
 
 const MainNavbar = () => {
 
     return(
-        <>
-            <Navbar bg="dark" variant="dark">
+        <div>
+            <Navbar className="navbar">
                 <Navbar.Brand href="/">
-                <img
-                    alt=""
-                    src={logo}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                />{' '}
-                What can I cook
+                    <img
+                        alt="logo"
+                        src={logo}
+                        width="50"
+                        height="50"
+                        className="d-inline-block"
+                    />{' '}
+                    <span className="title">What can I cook</span>
                 </Navbar.Brand>
+                <Form inline className="">
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-primary">Search</Button>
+                </Form>
+                
             </Navbar>
-        </>
+            
+            
+        </div>
     )
 }
 
